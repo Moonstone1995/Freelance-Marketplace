3b0f014f34dc0b6e9db6093eafd009c5c537d049
Michael Johnson <mikkojakko95@gmail.com>
2019-09-17 17:27:22 +0100

    My first successful posting attempt

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 $(document).ready(function() {
-  $('.input').click(function(event) {
-    
-    const firstName = $('#firstname').val();
-    const lastName = $('#lastname').val();
-    const userName = $('#username').val();
+  $('#input').click(function(event) {
+    event.preventDefault();
+    let firstname = $('#firstname').val();
+    let lastname = $('#lastname').val();
+    let username = $('#username').val();
     const gender = $('#inlineRadio').val();
     const email = $('#inputEmail').val();
     const password = $('#inputPassword').val();
-    const field = $('#fieldSelect').val();
-    const price = $('.form-check-label').val();
-    const details = $('.form-control2').val();
+    let field = $('#fieldSelect').val();
+    let price = $('.form-check-input').val();
+    let details = $('.form-control2').val();
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:3000/users',
+      data: {
+        firstname,
+        lastname,
+        username,
+        gender,
+        email,
+        password,
+        field,
+        price,
+        details,
+      },
+      success: function(){
+        alert('Registration Successful')
+      }
+   });
   });
 });
