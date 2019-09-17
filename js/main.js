d9fb82de56c785b91e61238d96ae44fe41483da6
Michael Johnson <mikkojakko95@gmail.com>
2019-09-17 15:34:30 +0100

    Testing jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
-})
+  $('.input').click(function(event) {
+    
+    const firstName = $('#firstname').val();
+    const lastName = $('#lastname').val();
+    const userName = $('#username').val();
+    const gender = $('#inlineRadio').val();
+    const email = $('#inputEmail').val();
+    const password = $('#inputPassword').val();
+    const field = $('#fieldSelect').val();
+    const price = $('.form-check-label').val();
+    const details = $('.form-control2').val();
+  });
+});
