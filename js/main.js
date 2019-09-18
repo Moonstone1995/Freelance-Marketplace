2de3f333c9c1a7867cca2a35a487cf7046622ad6
Michael Johnson <mikkojakko95@gmail.com>
2019-09-18 06:43:49 +0100

    Attempted to fix inconsistency in signup page

--- js/main.js
+++ js/main.js
@@ -4,29 +4,37 @@ $(document).ready(function() {
     let firstname = $('#firstname').val();
     let lastname = $('#lastname').val();
     let username = $('#username').val();
+    let state = $('#state').val();
+    let age = $('#age').val();
     const gender = $('#inlineRadio').val();
     const email = $('#inputEmail').val();
     const password = $('#inputPassword').val();
     let field = $('#fieldSelect').val();
-    let price = $('.form-check-input').val();
+    let price = $('#exampleRadios').val();
     let details = $('.form-control2').val();
-    $.ajax({
-      method: 'POST',
-      url: 'http://localhost:3000/users',
-      data: {
-        firstname,
-        lastname,
-        username,
-        gender,
-        email,
-        password,
-        field,
-        price,
-        details,
-      },
-      success: function(){
-        alert('Registration Successful')
-      }
-   });
+    if(!firstname){
+      alert("Fill up required fields")
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: 'http://localhost:3000/users',
+        data: {
+          firstname,
+          lastname,
+          username,
+          state,
+          age,
+          gender,
+          email,
+          password,
+          field,
+          price,
+          details,
+        },
+        success: function(){
+          alert('Registration Successful')
+        }
+      });
+   };
   });
 });
